33b0aefe2ff74286993162959f523d366548f516
yshaaang <erandiotrisha@gmail.com>
2023-01-13 21:29:27 +0800

    Updated multipleLineComment.ts
    used CHARACTER_WITHOUT_POUND instead

--- src/BNF/terminal/multipleLineComment.ts
+++ src/BNF/terminal/multipleLineComment.ts
@@ -1,16 +1,15 @@
 import BNF from "../../BNF";
-import { CHARACTER } from "./character";
+import { CHARACTER_WITHOUT_POUND } from "./characterWithoutPound";
 const { CONCAT, ATOM, STAR, } = BNF;
 
 export const MULTI_LINE_COMMENT = CONCAT(
     ATOM("#"),
     ATOM("#"),
     ATOM("#"),
-    STAR(CHARACTER),
+    STAR(CHARACTER_WITHOUT_POUND),
     ATOM("#"),
     ATOM("#"),
     ATOM("#"),
 )
     .name("MULTI_LINE_COMMENT")
     .token();
-    
