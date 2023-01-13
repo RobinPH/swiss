49aea76690f03c79eb7aa67c3f6679238cab663e
yshaaang <erandiotrisha@gmail.com>
2023-01-13 20:54:37 +0800

    Created multipleLineComment.ts

---
+++ src/BNF/terminal/multipleLineComment.ts
@@ -0,0 +1,15 @@
+import BNF from "../../BNF";
+import { CHARACTER } from "./character";
+const { CONCAT, ATOM, STAR, } = BNF;
+
+export const MULTI_LINE_COMMENT = CONCAT(
+    ATOM("#"),
+    ATOM("#"),
+    ATOM("#"),
+    STAR(CHARACTER),
+    ATOM("#"),
+    ATOM("#"),
+    ATOM("#"),
+)
+    .name("MULTI_LINE_COMMENT")
+    .token();
