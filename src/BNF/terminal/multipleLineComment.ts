4d7930887ea9e92b0eb82bf17f63d8c10433c0f9
RobinPH <robindimasin@gmail.com>
2023-01-14 14:56:11 +0800

    Optimized MULTI_LINE_COMMENT

--- src/BNF/terminal/multipleLineComment.ts
+++ src/BNF/terminal/multipleLineComment.ts
@@ -6,18 +6,7 @@ const THREE_POUND_BURGER = CONCAT(POUND, POUND, POUND);
 
 export const MULTI_LINE_COMMENT = CONCAT(
   THREE_POUND_BURGER,
-  STAR(
-    MINUS(
-      OR(
-        CHARACTER,
-        CONCAT(CHARACTER, CHARACTER),
-        CONCAT(CHARACTER, CHARACTER, CHARACTER)
-      ),
-      THREE_POUND_BURGER
-    )
-  )
-    .name("comment")
-    .token(),
+  STAR(MINUS(CHARACTER, THREE_POUND_BURGER)).name("COMMENT").token(),
   THREE_POUND_BURGER
 )
   .name("MULTI_LINE_COMMENT")
