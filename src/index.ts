e9a8ec186de444c2808676551e529624b9bd2241
Christian Roed Boyles <flldwthr@gmail.com>
2023-01-18 15:51:28 +0800

    Made a custom file extension for input
    We can now use index.sw to test the input for lexical analysis. The input string in index.ts was then deleted and moved to the swiss file instead.

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,12 @@
 import { TestResultStatus } from "./BNF/BaseBNF";
 import { toTable } from "./BNF/formatter";
 import { SWISS } from "./BNF/terminal/statement";
+import * as fs from 'fs';
+
+var read = fs.readFileSync('./src/index.sw', 'utf8');
 
 const bnf = SWISS;
-const input = `
-
-const GLOBAL_VARIABLE = 1;
-const SOMETHING = 1;
-const foo = 'str';
-let arr1 = [];
-let arr2 = [[1], 2, 3, 'str', [[[[]]]]];
-
-# comment
-
-### 
-  Multi-line Comment
-###
-
-function main() {
-  const int a = 1;
-  let b = 3.141519;
-  constant cint = "HELLO WORLD!";
-  const intc = "HELLO WORLD!";
-
-  function functiona(a, b) { # Hi
-    a++;
-  }
-
-  function func() {}
-
-  ### 
-  Multi-line Comment
-  ###
-
-  # Comment
-
-  ++a;
-
-  b += 1;
-  const exponent = 2 * (1 + 2);
-  b **= exponent;
-
-  if (a < 1) {
-    const d = 1;
-  } else {
-    if (b > 2) {
-      # code
-    } else if (d > 2) {
-      # code
-      a++;
-    } else {
-      const z = [];
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    const a = 1;
-
-    if (a > 1) {
-      b /= 1;
-    }
-  }
-
-  while (b > 0) {
-    # code
-  }
-
-  do {
-     # code
-  } while (c > 0);
-}
-
-`;
+const input = read;
 
 const res = bnf.test(input);
 
@@ -83,9 +18,9 @@ if (res.status === TestResultStatus.SUCCESS) {
 
 toTable("symbol_table.txt", res);
 
-for (const b of [bnf]) {
+/*for (const b of [bnf]) {
   console.log(b.toDeclaration());
-}
+}*/
 
 // @ts-ignore
 // vizualize("res", bnf);
