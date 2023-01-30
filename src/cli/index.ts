d32274f1afd79c11763284c1e1ddb5d1514748f6
RobinPH <robindimasin@gmail.com>
2023-01-31 00:34:50 +0800

    Added testInput function

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 import { TestResultStatus } from "../BNF/BaseBNF";
 import { toTable } from "../BNF/formatter";
 import { SWISS } from "../BNF/terminal/statement";
-import { getCode } from "./utility";
+import { getCode, testInput } from "./utility";
 
 const main = async () => {
   if (process.argv.length < 3) {
@@ -18,49 +18,7 @@ const main = async () => {
     return;
   }
 
-  try {
-    var code = getCode(filepath);
-  } catch (e: any) {
-    console.log(e.message);
-    return;
-  }
-
-  const bnf = SWISS;
-  console.log("Processing...");
-  const result = await bnf.test(code);
-
-  const filename = path.parse(path.basename(filepath)).name;
-  const symbolTableFileName = `${filename}.symbol`;
-
-  if (result.status === TestResultStatus.SUCCESS) {
-    console.log("[?] SUCCESS");
-  } else if (result.status === TestResultStatus.FAILED) {
-    console.log(`[!] FAILED`);
-
-    // const startOffest = 20;
-    // const start = Math.max(0, result.range.to - startOffest);
-    // const end = result.range.to + 40;
-    // const segment = code.slice(start, end);
-    // const snippet = segment.split("\n").join(" ");
-    // const lineNumber = code.slice(0, result.range.to + 1).split("\n").length;
-    // let columnNumber = 0;
-
-    // for (let i = 0; i < result.range.to; i++) {
-    //   if (code[i] === "\n") {
-    //     columnNumber = 0;
-    //     continue;
-    //   }
-    //   columnNumber++;
-    // }
-    // const prefix = `[Ln ${lineNumber}, Col ${columnNumber + 1}]:`;
-    // console.log(`${prefix} ${snippet}`);
-    // console.log(
-    //   "".padStart(startOffest + lineNumber + prefix.length) +
-    //     "^ Error started here"
-    // );
-  }
-
-  toTable(symbolTableFileName, result);
+  await testInput(SWISS, filepath);
 };
 
 main();
